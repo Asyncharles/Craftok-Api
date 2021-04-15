97d9915ea7a15745df61e5dc02046e7ccf384e30
FinalVoid <finalvoiddev@gmail.com>
2021-04-15 23:53:06 +0200

    Updated API VERSION

--- src/main/java/eu/craftok/api/CraftokProvider.java
+++ src/main/java/eu/craftok/api/CraftokProvider.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 public class CraftokProvider {
     private static CraftokApi instance = null;
     private final static Logger LOGGER = Logger.getLogger("PROVIDER");
-    public final static String API_VERSION = "0.1.9";
+    public final static String API_VERSION = "0.1.10";
 
     private CraftokProvider() {
     }
