4d7e78cf7564b7b7209b080d5180bf0f08ca9baf
FinalVoid <finalvoiddev@gmail.com>
2021-04-22 00:30:09 +0200

    Added support for all managers

--- src/main/java/eu/craftok/api/CraftokApi.java
+++ src/main/java/eu/craftok/api/CraftokApi.java
@@ -1,6 +1,9 @@
 package eu.craftok.api;
 
+import eu.craftok.api.punishments.PunishmentManager;
 import eu.craftok.api.rank.GroupManager;
+import eu.craftok.api.staff.StaffOptionManager;
+import eu.craftok.api.staff.reports.ReportHandler;
 import eu.craftok.api.user.UserManager;
 
 public interface CraftokApi {
@@ -15,4 +18,22 @@ public interface CraftokApi {
      * @return the {@link UserManager} instance
      */
     UserManager getUserManager();
+
+    /**
+     *
+     * @return the {@link StaffOptionManager} instance
+     */
+    StaffOptionManager getStaffOptionManager();
+
+    /**
+     *
+     * @return the {@link PunishmentManager} instance
+     */
+    PunishmentManager getPunishmentManager();
+
+    /**
+     *
+     * @return the {@link ReportHandler} instance
+     */
+    ReportHandler getReportHandler();
 }
