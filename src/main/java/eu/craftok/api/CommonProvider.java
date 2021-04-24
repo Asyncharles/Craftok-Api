ef0e84e6493c1ef4c37aa95465cfedc22625fa29
FinalVoid <finalvoiddev@gmail.com>
2021-04-25 00:05:46 +0200

    Updated API VERSION

--- src/main/java/eu/craftok/api/CommonProvider.java
+++ src/main/java/eu/craftok/api/CommonProvider.java
@@ -6,15 +6,29 @@ public class CommonProvider {
     private final boolean nameTagOverride;
     private final boolean chatFormatOverride;
     private final boolean activeDatabaseActor;
-    private final boolean activeCacheActor;
+    private final boolean activeCacheHandler;
+    private final boolean logApiResponse;
+    private final int cacheHandlerTimer;
+    private final int nThreadLog;
 
-    public CommonProvider(CraftokApi api, final boolean tabOverride, final boolean nameTagOverride, final boolean chatFormatOverride, final boolean activeDatabaseActor, final boolean activeCacheActor) {
+    public CommonProvider(CraftokApi api,
+                          final boolean tabOverride,
+                          final boolean nameTagOverride,
+                          final boolean chatFormatOverride,
+                          final boolean activeDatabaseActor,
+                          final boolean activeCacheHandler,
+                          final boolean logApiResponse,
+                          final int cacheHandlerTimer,
+                          final int nThreadLog) {
         this.api = api;
         this.tabOverride = tabOverride;
         this.nameTagOverride = nameTagOverride;
         this.chatFormatOverride = chatFormatOverride;
         this.activeDatabaseActor = activeDatabaseActor;
-        this.activeCacheActor = activeCacheActor;
+        this.activeCacheHandler = activeCacheHandler;
+        this.logApiResponse = logApiResponse;
+        this.cacheHandlerTimer = cacheHandlerTimer;
+        this.nThreadLog = nThreadLog;
     }
 
     /**
@@ -61,7 +75,31 @@ public class CommonProvider {
      *
      * @return {@code true} if the server is an active cache actor, {@code false} otherwise
      */
-    public boolean isActiveCacheActor() {
-        return activeCacheActor;
+    public boolean isActiveCacheHandler() {
+        return activeCacheHandler;
+    }
+
+    /**
+     *
+     * @return {@code true} to log the api response time, {@code false} otherwise
+     */
+    public boolean hasLogApiResponse() {
+        return logApiResponse;
+    }
+
+    /**
+     *
+     * @return the cache update timer
+     */
+    public int getCacheHandlerTimer() {
+        return cacheHandlerTimer;
+    }
+
+    /**
+     *
+     * @return the amount of thread available to log the api response time
+     */
+    public int getNThreadLog() {
+        return nThreadLog;
     }
 }
