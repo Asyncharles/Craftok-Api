72831ba7d4a8b14b2d93b33a6b0d785e7d8b8971
FinalVoid <finalvoiddev@gmail.com>
2021-04-25 00:06:11 +0200

    Updated Api Builder

--- src/main/java/eu/craftok/api/CraftokApiBuilder.java
+++ src/main/java/eu/craftok/api/CraftokApiBuilder.java
@@ -6,6 +6,9 @@ public class CraftokApiBuilder {
     private boolean chatFormatOverride = false;
     private boolean activeDatabaseActor = false;
     private boolean activeCacheHandler =  false;
+    private boolean logApiResponse = false;
+    private int cacheHandlerTimer = 0;
+    private int nThreadLog = 1;
 
     public CraftokApiBuilder() {
     }
@@ -30,12 +33,19 @@ public class CraftokApiBuilder {
         return this;
     }
 
-    public CraftokApiBuilder setActiveCacheHandler() {
+    public CraftokApiBuilder setActiveCacheHandler(final int timer) {
         activeCacheHandler = true;
+        cacheHandlerTimer = timer;
+        return this;
+    }
+
+    public CraftokApiBuilder logApiResponse(final int nThread) {
+        logApiResponse = true;
+        nThreadLog = nThread;
         return this;
     }
 
     public CraftokApi create() {
-        return new CraftokProvider(tabOverride, nameTagOverride, chatFormatOverride, activeDatabaseActor, activeCacheHandler).getApi();
+        return new CraftokProvider(tabOverride, nameTagOverride, chatFormatOverride, activeDatabaseActor, activeCacheHandler, logApiResponse, cacheHandlerTimer, nThreadLog).getApi();
     }
 }
