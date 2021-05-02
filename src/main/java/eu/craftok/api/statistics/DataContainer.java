45890d205df0d586ad1b7af9495f6856c85b4f99
FinalVoid <finalvoiddev@gmail.com>
2021-05-02 12:01:31 +0200

    Added Statistics

---
+++ src/main/java/eu/craftok/api/statistics/DataContainer.java
@@ -0,0 +1,52 @@
+package eu.craftok.api.statistics;
+
+public interface DataContainer {
+    /**
+     * Get a statistic
+     * @param key the key of the statistic
+     * @param c the type of the statistic
+     * @param <T> the generic type
+     * @return the statistic
+     */
+    <T> T get(String key, T c);
+
+    /**
+     * Check if the statistic exists
+     * @param key the key of the statistic
+     * @param <T> the generic type
+     * @return {@code true} if the statistic exists, {@code false} otherwise
+     */
+    <T> boolean has(String key);
+
+    /**
+     * Check if the statistic exists
+     * @param key the of the statistic
+     * @param c the type of the statistic
+     * @param <T> the generic type
+     * @return {@code true} if the statistic exists, {@code false} otherwise
+     */
+    <T> boolean has(String key, T c);
+
+    /**
+     * Add a statistic
+     * @param key the key of the statistic
+     * @param c the type of the statistic
+     * @param <T> the generic type
+     */
+    <T> void add(String key, T c);
+
+    /**
+     * Remove a statistic
+     * @param key the of the statistic
+     */
+    void remove(String key);
+
+    /**
+     * Remove a statistic
+     * @param key the key of the statistic
+     * @param c the type of the statistic
+     * @param <T> the generic type
+     * @return the {@link T} statistic removed
+     */
+    <T> T remove(String key, T c);
+}
