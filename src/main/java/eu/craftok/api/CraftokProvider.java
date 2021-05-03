868e700a238eb6d915093ce5df61e44878f598ae
FinalVoid <finalvoiddev@gmail.com>
2021-05-03 19:51:02 +0200

    Updated API VERSION

--- src/main/java/eu/craftok/api/CraftokProvider.java
+++ src/main/java/eu/craftok/api/CraftokProvider.java
@@ -18,7 +18,7 @@ public class CraftokProvider {
     private static int cacheHandlerTimer;
     private static int nThreadLog;
 
-    public final static String API_VERSION = "0.3.2";
+    public final static String API_VERSION = "0.3.3";
 
     protected CraftokProvider(boolean tabOverride, boolean nameTagOverride, boolean chatFormatOverride, boolean activeDatabaseActor, boolean activeCacheHandler, boolean logApiResponse, int cacheHandlerTimer, int nThreadLog) {
         CraftokProvider.tabOverride = tabOverride;
