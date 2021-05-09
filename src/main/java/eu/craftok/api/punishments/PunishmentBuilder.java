748e7ec942c86455373befa368d3e2173047e61c
FinalVoid <finalvoiddev@gmail.com>
2021-05-09 20:34:11 +0200

    Updated PunishmentBuilder

--- src/main/java/eu/craftok/api/punishments/PunishmentBuilder.java
+++ src/main/java/eu/craftok/api/punishments/PunishmentBuilder.java
@@ -1,5 +1,6 @@
 package eu.craftok.api.punishments;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class PunishmentBuilder {
@@ -20,31 +21,64 @@ public class PunishmentBuilder {
         this.issuedDate = System.currentTimeMillis();
     }
 
-    public void setIssuerUuid(UUID issuerUuid) {
+    public PunishmentBuilder setIssuerUuid(UUID issuerUuid) {
         this.issuerUuid = issuerUuid;
+        return this;
     }
 
-    public void setTargetUUid(UUID targetUUid) {
+    public PunishmentBuilder setTargetUUid(UUID targetUUid) {
         this.targetUUid = targetUUid;
+        return this;
     }
 
-    public void setIssuedDate(long issuedDate) {
+    public PunishmentBuilder setIssuedDate(long issuedDate) {
         this.issuedDate = issuedDate;
+        return this;
     }
 
-    public void setExpiryTime(long expiryTime) {
+    public PunishmentBuilder setExpiryTime(long expiryTime) {
         this.expiryTime = expiryTime;
+        return this;
     }
 
-    public void setReason(String reason) {
+    public PunishmentBuilder setReason(String reason) {
         this.reason = reason;
+        return this;
     }
 
-    public void setPunishmentType(Punishment.PunishmentType punishmentType) {
+    public PunishmentBuilder setPunishmentType(Punishment.PunishmentType punishmentType) {
         this.punishmentType = punishmentType;
+        return this;
     }
 
     public PunishmentBuilder build() {
+        if (issuerUuid == null || targetUUid == null || reason == null || punishmentType == null) {
+            return null;
+        }
         return this;
+    }
+
+    public UUID getIssuerUuid() {
+        return issuerUuid;
+    }
+
+    public UUID getTargetUUid() {
+        return targetUUid;
+    }
+
+    public long getIssuedDate() {
+        return issuedDate;
+    }
+
+    public long getExpiryTime() {
+        return expiryTime;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public Punishment.PunishmentType getPunishmentType() {
+        return punishmentType;
     }
 }
